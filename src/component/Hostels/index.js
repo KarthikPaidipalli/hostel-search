45ed3fd9bea8a165d7abf0769a99787990ef27be
KarthikPaidipalli <paidipallikarthik1510@gmail.com>
2024-02-21 12:03:28 +0530

    third

--- src/component/Hostels/index.js
+++ src/component/Hostels/index.js
@@ -156,6 +156,14 @@ const listofholstels=[
       sharing:3,
       rent:7500
   }
+  {
+    id:20,
+    type:"Co-Living",
+    hostelname:"Tarun Coliving",
+    image:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR855_8G0UWHGYeuH94zK40S8l6sa-nBdsN1g&usqp=CAU",
+    sharing:4,
+    rent:7500
+}
 ]
 const ratings=[
     {
